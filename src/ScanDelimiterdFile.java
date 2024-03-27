f23b33c561339bca20fe3e2a27d080d7e4f943ed
seddonnguyen <tri.seddon.nguyen@gmail.com>
2024-03-27 17:30:41 -0600

    Add project files

--- src/ScanDelimiterdFile.java
+++ src/ScanDelimiterdFile.java
@@ -2,48 +2,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
-<<<<<<< HEAD
-import java.util.List;
-=======
-<<<<<<< HEAD
-import java.util.List;
-=======
->>>>>>> f20a93c (Add project files)
->>>>>>> 7ba78d4 (Add project files)
 
 public class ScanDelimiterdFile {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 7ba78d4 (Add project files)
-        try {
-            String location = "cars.csv";
-            File file = new File(location);
-            Scanner input = new Scanner(file);
-            List<String[]> data = new ArrayList<>();
-
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                String[] values = line.split(",");
-                data.add(values);
-            }
-            input.close();
-
-            for (String[] line : data) {
-                System.out.println(STR."Car Name: \{line[0]}");
-                System.out.println(STR."MPG: \{line[1]}");
-                System.out.println(STR."Cylinders: \{line[2]}");
-                System.out.println(STR."Displacement: \{line[3]}");
-                System.out.println(STR."Horsepower: \{line[4]}");
-                System.out.println(STR."Weight: \{line[5]}");
-                System.out.println(STR."Acceleration: \{line[6]}");
-                System.out.println(STR."Model: \{line[7]}");
-                System.out.println(STR."Origin: \{line[8]}");
-<<<<<<< HEAD
-=======
-=======
         try (var input = new Scanner(new File("cars.csv"))) {
             var cars = new ArrayList<String[]>();
 
@@ -63,8 +25,6 @@ public class ScanDelimiterdFile {
                 System.out.println(STR."Acceleration: \{car[6]}");
                 System.out.println(STR."Model: \{car[7]}");
                 System.out.println(STR."Origin: \{car[8]}");
->>>>>>> f20a93c (Add project files)
->>>>>>> 7ba78d4 (Add project files)
                 System.out.println("===============================");
             }
 
--- src/myRunner.java
+++ src/myRunner.java
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 7ba78d4 (Add project files)
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
-
-public class myRunner {
-    public static void main(String[] args) {
-        try {
-            File file = new File("CourseData.csv");
-            Scanner input = new Scanner(file);
-            List<course> data = new ArrayList<>();
-
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                String[] values = line.split(",");
-                course c = new course();
-                c.setCode(values[0]);
-                c.setCourse_name(values[1]);
-                c.setInstructor_name(values[2]);
-                data.add(c);
-            }
-            input.close();
-
-            for (course c : data) {
-                System.out.println(STR."\{c.getCode()} | \{c.getCourse_name()} | \{c.getInstructor_name()}");
-                System.out.println("===============================");
-            }
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found.");
-            e.printStackTrace();
-        }
-    }
-}
-<<<<<<< HEAD
-=======
-=======
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -70,6 +28,3 @@ public class myRunner {
         }
     }
 }
-
->>>>>>> f20a93c (Add project files)
->>>>>>> 7ba78d4 (Add project files)
